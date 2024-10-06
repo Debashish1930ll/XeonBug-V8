bc191fa0a04558710a19a5dfbb3ac32fbf54f4d2
Debashish1930ll <mithunshill41@gmail.com>
2024-10-06 16:40:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "918972897084"
+global.ownername = "Niloy"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '918972897084'  //creator number
+global.ownername = 'Niloy' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
